721d1ac866ff9687ef72222c38802d516e338f4b
szhengyang <zysng@mit.edu>
2018-04-04 10:32:14 -0400

    first commit

--- exercise.js
+++ exercise.js
@@ -5,7 +5,11 @@ exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
     //-------------------    
-    return 'Error: 1st function not implemented';    
+    var x_dist_squared = Math.pow((p1.x - p2.x),2);
+    var y_dist_squared = Math.pow((p1.y - p2.y),2);
+    var dist = Math.sqrt(x_dist_squared + y_dist_squared);
+    
+    return dist;    
 };
 
 // calculate distance between new point and existing
@@ -13,7 +17,14 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+
+    for (i=0;i<data.length;i++){
+        var new_x_dist_squared = Math.pow((data[i].x-newPoint.x),2);
+        var new_y_dist_squared = Math.pow((data[i].y-newPoint.y),2);
+        var new_dist = Math.sqrt(new_x_dist_squared + new_y_dist_squared);
+        data[i].distance = new_dist;
+    }
+    return data;
 };
 
 // find k closest points
@@ -21,7 +32,10 @@ exercise.findClosest = function(k,data){
     //-------------------
     //---- Your Code ----
     //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+        return a.distance - b.distance;
+    });
+    return data.slice(0,k); 
 };
 
 module.exports = exercise;
